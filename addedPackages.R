af5e02404acda1df173e39cec1f093d34b3553f1
Leonard Greski <len@ageretechnology.com>
2015-12-10 20:35:14 -0800

    ran entire model on x360

---
+++ addedPackages.R
@@ -0,0 +1,9 @@
+install.packages("pbkrtest",type="win.binary")
+install.packages("caret")
+
+install.packages(pkgs = "caret", 
+                 dependencies = c("Depends", "Imports"))
+install.packages("YaleToolkit")
+install.packages("e1071")
+install.packages("doParallel")
+install.packages("randomForest")
